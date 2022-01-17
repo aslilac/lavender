c7d2e0f084f30e3dd0e99ab8c1789fd7812a146d
McKayla Washburn <mckayla@hey.com>
2022-01-16 18:31:06 -0700

    oh boy

---
+++ build-book.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rust-lang/mdBook/releases/download/v0.4.15/mdbook-v0.4.15-x86_64-unknown-linux-gnu.tar.gz && \
+	tar -xf mdbook-v0.4.15-x86_64-unknown.tar.gz && \
+	./mdbook ./lavender-book
